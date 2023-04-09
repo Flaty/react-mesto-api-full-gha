b23fdb20ae8f76a6234eca1eb3325f906ffae4fa
kaneki <hitomi.jpeg@gmail.com>
2023-04-09 12:41:15 +0300

    update app.js

--- backend/app.js
+++ backend/app.js
@@ -1,46 +1,51 @@
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
-const helmet = require('helmet');
 const cors = require('cors');
+const helmet = require('helmet');
 const rateLimit = require('express-rate-limit');
 const bodyParser = require('body-parser');
 const { errors } = require('celebrate');
 const router = require('./routes');
 const auth = require('./middlewares/auth');
 const { MONGO_URL } = require('./config');
+const corsErr = require('./middlewares/corsErr');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const { PORT = 3000 } = process.env;
 const app = express();
-
+app.use(cors());
 app.use(bodyParser.json());
 const { validationCreateUser, validationLogin } = require('./middlewares/validation');
 
 const { createUsers, login } = require('./controllers/auth');
-app.use(cors());
-app.use(helmet());
+
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000,
   max: 100,
   standardHeaders: true,
   legacyHeaders: false,
 });
-app.use(limiter);
-
+app.use(requestLogger); 
+app.post('/signin', validationLogin, login);
+app.post('/signup', validationCreateUser, createUsers);
+app.use(auth);
 app.get('/crash-test', () => {
   setTimeout(() => {
     throw new Error('Сервер сейчас упадёт');
   }, 0);
 });
-
-app.post('/signin', validationLogin, login);
-app.post('/signup', validationCreateUser, createUsers);
-app.use(auth);
 app.use(router);
+app.use(helmet());
+app.use(limiter);
+app.use(errorLogger); 
+app.use(corsErr); 
 async function connect() {
   try {
-    await mongoose.connect(MONGO_URL);
     await mongoose.set('strictQuery', false);
-    await mongoose.connect('mongodb://localhost:27017/mestodb');
+    await mongoose.connect(MONGO_URL, {
+      useNewUrlParser: true,
+    });
     console.log(`App connected ${MONGO_URL}`);
     await app.listen(PORT);
     console.log(`App listening on port ${PORT}`);
@@ -60,3 +65,4 @@ app.use((err, req, res, next) => {
 });
 
 connect();
+
